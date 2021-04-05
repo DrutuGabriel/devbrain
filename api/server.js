0313cb3673c7b40d22a7d135a13fca1cd4b274a8
Gabriel D <gabriel.drutu@gmail.com>
2021-04-05 17:58:52 +0300

    Changes for deployment

--- api/server.js
+++ api/server.js
@@ -5,10 +5,14 @@ const cors = require('cors');
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host: 'mydb',
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+    // host: process.env.POSTGRES_HOST,
+    // user: process.env.POSTGRES_USER,
+    // password: process.env.POSTGRES_PASSWORD,
+    // database: process.env.POSTGRES_DB,
   },
 });
 
